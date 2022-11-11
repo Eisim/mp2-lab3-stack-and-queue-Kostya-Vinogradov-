0398eb76f15ca36d053e5041aaa0cee847f82908
Eisim <koctik.w@mail.ru>
2022-11-11 22:43:53 +0300

    working code

--- include/queue.h
+++ include/queue.h
@@ -5,17 +5,77 @@ class Queue {
 	T* data;
 	size_t size;
 	size_t capacity;
-	void resize(const int& size);
+	size_t lptr, rptr;
 
+	void resize() {
+		capacity = (size + 1) * 2;
+		T* tmp = new T[capacity];
+		std::copy(data, data + size, tmp);
+		delete[] data;
+		data = tmp;
+		lptr = 0;
+		rptr = size;
+	}
 public:
-	Queue();
-	Queue(const Queue& q);
-	~Queue();
+	Queue() {
+		size = 0;
+		capacity = 1;
+		data = new T[capacity];
+		lptr = 0;
+		rptr = 0;
+	}
+	Queue(const Queue& q) {
+		lptr = q.lptr;
+		rptr = q.rptr;
+		size = q.size;
+		capacity = q.capacity;
+		data = new T[capacity];
+		std::copy(q.data, q.data + q.size, data);
+	}
+	~Queue() {
+		lptr = 0;
+		rptr = 0;
+		size = 0;
+		capacity = 0;
+		delete[] data;
+	}
 	
-	Queue& operator=(const Queue& q);
+	Queue& operator=(const Queue& q) {
+		if (this == &q) return *this;
+		size = q.size;
+		capacity = q.capacity;
+		lptr = q.lptr;
+		rptr = q.rptr;
+		delete[] data;
+		data = new T[capacity];
+		std::copy(q.data, q.data + q.size, data);
 
-	bool empty();
-	T push(T elem);
-	void pop();
-	T top();
+		return *this;
+	}
+
+	bool empty() { return !size; }
+
+	void push(T elem) {
+		data[rptr] = elem;
+		rptr++;
+		size++;
+		if (rptr >= capacity)rptr = 0;
+		if (rptr == lptr && size != 0) resize();
+
+	}
+	void pop() {
+		data[lptr] = 0;
+		lptr++;
+		if (lptr >=capacity)lptr = 0;
+		if (size == 0 || lptr > rptr) throw std::exception("Queue is empty");
+		size--;
+
+	}
+
+	size_t getSize() { return size; }
+
+	T top() {
+		if(size<=0)throw std::exception("Queue is empty");
+		return data[lptr];
+	}
 };
--- include/stack.h
+++ include/stack.h
@@ -5,17 +5,67 @@ class Stack {
 	T* data;
 	size_t size;
 	size_t capacity;
-	void resize(const int& size);
+
+	void resize() {
+		capacity = (size + 1) * 2;
+		T* tmp=new T[capacity];
+		std::copy(data,data+size,tmp);
+		delete[] data;
+		data = tmp;
+	}
 
 public:
-	Stack();
-	Stack(const Stack& st);
-	~Stack();
+	Stack() {
+		size = 0;
+		capacity = 1;
+		data = new T[capacity];
+	}
 
-	Stack& operator=(const Stack& q);
+	Stack(const Stack& st) {
+		size = st.size;
+		capacity = st.capacity;
+		data = new T[capacity];
+		std::copy(st.data, st.data + st.size, data);
 
-	bool empty();
-	T push(T elem);
-	void pop();
-	T top();
+
+	}
+	~Stack() {
+		size = 0;
+		capacity = 0;
+		delete[] data;
+	}
+
+	Stack& operator=(const Stack st) {
+		if (this == &st) return *this;
+		size = st.size;
+		capacity = st.capacity;
+
+		delete[] data;
+		data = new T[capacity];
+		std::copy(st.data, st.data + st.size, data);
+
+
+		return *this;
+	}
+
+	bool empty() { return !size; }
+	void push(T elem) {
+		if (size  >= capacity) resize();
+		data[size] = elem;
+		size++;
+	}
+	
+	void pop() {
+		if ((size) <= 0) throw std::exception("Stack is empty");
+		
+		size-=1;
+		data[size] = 0;
+		
+	}
+	size_t getSize() { return size; }
+
+	T top() {
+		if (size <= 0) throw std::exception("Stack is empty");
+		return data[size-1];
+	}
 };
